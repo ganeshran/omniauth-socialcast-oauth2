85618ccdc613e93ae1d9827dff9428e6bb532ff9
ganeshran <ganesh.ranganathan.85@gmail.com>
2013-08-18 04:13:24 +0530

    adding avatar info

--- lib/omniauth/strategies/socialcast.rb
+++ lib/omniauth/strategies/socialcast.rb
@@ -1,4 +1,4 @@
-require 'omniauth-oauth2'
+load 'omniauth-oauth2'
 
 module Omniauth
 	module Strategies
@@ -9,25 +9,23 @@ module Omniauth
         # This is where you pass the options you would pass when
       # initializing your consumer from the OAuth gem.
       option :client_options, {
-      	#:site => (ENV['SOCIALCAST_DOMAIN'] || 'https://demo.socialcast.com'),
-      	#:site => (ENV['SOCIALCAST_DOMAIN']),
       	:site => nil,
       	:authorize_url => '/oauth2/authorization?response_type=code',
       	:token_url => '/oauth2/token'
       }
 
-      # These are called after authentication has succeeded. If
-      # possible, you should try to set the UID without making
-      # additional calls (if the user id is returned with the token
-      # or as a URI parameter). This may not be possible with all
-      # providers.
       uid{ raw_info['user']['id'] }
 
      info do
      	{
      		:name => raw_info['user']['name']
-            #Rails.logger.debug(:name)
-     		#:email => raw_info['email']
+        :avatars => {
+                      :16 => ['user']['avatar']['square16']
+                      :30 => ['user']['avatar']['square30']
+                      :45 => ['user']['avatar']['square45']
+                      :70 => ['user']['avatar']['square70']
+                      :140 => ['user']['avatar']['square140']
+                    }
      	}
      end
 
